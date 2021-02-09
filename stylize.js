d135e38aaa2a7ad86e62a16cf6c2e07c936b01a8
cgcostume <cgdaniel@outlook.com>
2021-02-09 16:40:17 +0100

    integrate stylize sdk

---
+++ stylize.js
@@ -0,0 +1,49 @@
+
+const axios = require('axios');
+
+const glob = require('glob');
+const fs = require('fs');
+
+const FormData = require('form-data');
+
+
+const MAX_IMAGE_SIZE = 5000000;
+
+async function stylize(imagePath) {
+
+    const image = {
+        data: fs.createReadStream(imagePath),
+        mimetype: 'image/jpeg',
+        name: imagePath,
+    };
+
+    const data = new FormData({ maxDataSize: MAX_IMAGE_SIZE });
+    data.append('image', image.data, image.name);
+
+    const headers = {
+        'ApiKeyId': `${ process.env.STYLIZATION_SDK_API_KEY_ID }`,
+        'ApiKey': `${ process.env.STYLIZATION_SDK_API_KEY }`,
+    };
+    const pipeline = [{
+        'effectId': 'default/toon/definition/toon/1',
+        'preset': 'landscape',
+    }];
+    data.append('pipeline', JSON.stringify(pipeline));
+
+    const response = await axios({
+        method: 'post',
+        url: `${ process.env.STYLIZATION_SDK_API_URL }${ process.env.STYLIZATION_SDK_API_ROUTE }`,
+        data: data,
+        headers: Object.assign(Object.assign({}, headers), data.getHeaders()),
+        responseType: 'stream',
+    }).then(function (response) {
+        response.data.pipe(fs.createWriteStream(imagePath));
+    });
+}
+
+
+glob("source/img/**/header-*.jpg", function (er, files) {
+    files.forEach((value, index) => {
+        stylize(value);
+    });
+});

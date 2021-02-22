c46e4fc34f84826693344d6a66499b8de5548bc0
cgcostume <cgdaniel@outlook.com>
2021-02-22 13:07:46 +0100

    revert stylization of portraits (for now)

--- stylize.js
+++ stylize.js
@@ -53,9 +53,3 @@ glob("source/img/**/header-*.jpg", function (er, files) {
         stylize(value);
     });
 });
-
-glob("source/img/**/portrait-*.jpg", function (er, files) {
-    files.forEach((value, index) => {
-        stylize(value);
-    });
-});
